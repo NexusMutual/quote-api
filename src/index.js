f8c7046bc3432ce843e1fe492ac6c8705f31046c
shark0der <tolean.dj@gmail.com>
2020-01-27 21:54:53 +0200

    Initialize quote engine

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 const app = require('express')();
 const mongoose = require('mongoose');
+const fetch = require('node-fetch');
 
-const DB = require('./db');
 const Etherscan = require('./etherscan');
-const Stake = require('./models/stake');
+const QuoteEngine = require('./quote-engine');
 
 const {
   API_URL,
@@ -15,30 +15,30 @@ const {
 } = process.env;
 
 const etherscan = new Etherscan('ETHERSCAN_API_KEY');
+const quoteEngine = new QuoteEngine(etherscan);
 
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider(PROVIDER_URL));
 
 let abis;
 
-async function startServer (app, port) {
-  return new Promise(resolve => app.listen(port, resolve));
-}
-
 async function fetchVersionData (chain = 'mainnet') {
   const versionDataURL = `${API_URL}/version-data/data.json`;
   const data = await fetch(versionDataURL).then(res => res.json());
 
   if (typeof data[chain] === 'undefined') {
-    throw new Error(`No data for version ${version} found.`);
+    throw new Error(`No data for ${chain} chain found.`);
   }
 
   return data[chain].abis;
 }
 
-async function init () {
+async function startServer (app, port) {
+  return new Promise(resolve => app.listen(port, resolve));
+}
 
-  mongoose.connect(MONGO_URL, { useNewUrlParser: true });
+async function init () {
+  await mongoose.connect(MONGO_URL, { useNewUrlParser: true });
 
   console.log('Fetching version data');
   abis = await fetchVersionData();
