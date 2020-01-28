0a076853d6e193ed1efb3bb599df3ebe238116c3
shark0der <tolean.dj@gmail.com>
2020-01-28 16:37:49 +0200

    Add express routes

---
+++ src/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+
+const asyncRoute = route => (req, res) => {
+  route(req, res).catch(e => {
+    console.error(e);
+    res.status(500).send({
+      error: true,
+      message: 'Internal server error',
+    });
+  });
+};
+
+/**
+ * @param {QuoteEngine} quoteEngine
+ * @return {app}
+ */
+module.exports = quoteEngine => {
+
+  const app = express();
+
+  app.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl}`);
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'x-api-key');
+    next();
+  });
+
+  app.get('/getQuote/:coverAmount/:currency/:period/:contractAddress/:version', asyncRoute(async (req, res) => {
+
+    const origin = req.get('origin');
+    const apiKey = req.headers['x-api-key'];
+    const isAllowed = await quoteEngine.isOriginAllowed(origin, apiKey);
+
+    if (!isAllowed) {
+      return res.status(403).send({
+        error: true,
+        message: 'Origin not allowed. Contact us for an API key',
+      });
+    }
+
+    const { contractAddress, coverAmount, currency, period } = req.params;
+    const quote = await quoteEngine.getQuote(contractAddress, coverAmount, currency, period);
+
+    res.send(quote);
+  }));
+
+  return app;
+};
