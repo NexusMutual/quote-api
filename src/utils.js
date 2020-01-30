a077e2faa130dd3a901da433f3ff4f028ac17920
shark0der <tolean.dj@gmail.com>
2020-01-30 20:53:12 +0200

    Implement utils.min and utils.max for big numbers

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,37 @@
+const Big = require('big.js');
+
 function wrap (text, length) {
   const regex = new RegExp(`.{1,${length}}`, 'g');
   return text.match(regex);
 }
 
+/**
+ * Returns the minimum of two numbers
+ * Input can be big.js instance, string, or number
+ * @param {(Big|string|number)} a
+ * @param {(Big|string|number)} b
+ * @return {Big}
+ */
+function min (a, b) {
+  const bigA = Big(a);
+  const bigB = Big(b);
+  return bigA.lt(bigB) ? bigA : bigB;
+}
+
+/**
+ * Returns maximum of two big.js numbers
+ * @param {(Big|string|number)} a
+ * @param {(Big|string|number)} b
+ * @return {Big}
+ */
+function max (a, b) {
+  const bigA = Big(a);
+  const bigB = Big(b);
+  return bigA.gt(bigB) ? bigA : bigB;
+}
+
 module.exports = {
   wrap,
+  min,
+  max,
 };
--- test/utils.js
+++ test/utils.js
@@ -1,4 +1,6 @@
 const assert = require('assert');
+const Big = require('big.js');
+
 const utils = require('../src/utils');
 
 describe('utils', function () {
@@ -12,4 +14,57 @@ describe('utils', function () {
     });
 
   });
+
+  describe('min()', function () {
+
+    it('should return a big.js instance', function () {
+      const actual = utils.min(1, 2);
+      assert.strict(actual instanceof Big);
+    });
+
+    it('should return the min of two numbers', function () {
+      const actual = utils.min(1, 2);
+      assert.strict(actual.eq(1));
+    });
+
+    it('should return the min of two numbers passed as string', function () {
+      const actual = utils.min('1', '2');
+      assert.strict(actual.eq(1));
+    });
+
+    it('should return the min of two numbers passed as big.js instances', function () {
+      const a = Big(1);
+      const b = Big(2);
+      const actual = utils.min(a, b);
+      assert.strict(actual.eq(1));
+    });
+
+  });
+
+  describe('max()', function () {
+
+    it('should return a big.js instance', function () {
+      const actual = utils.max(1, 2);
+      assert.strict(actual instanceof Big);
+    });
+
+    it('should return the max of two numbers', function () {
+      const actual = utils.max(1, 2);
+      assert.strict(actual.eq(2));
+    });
+
+    it('should return the max of two numbers passed as string', function () {
+      const actual = utils.max('1', '2');
+      assert.strict(actual.eq(2));
+    });
+
+    it('should return the max of two numbers passed as big.js instances', function () {
+      const a = Big(1);
+      const b = Big(2);
+      const actual = utils.max(a, b);
+      assert.strict(actual.eq(2));
+    });
+
+  });
+
 });
