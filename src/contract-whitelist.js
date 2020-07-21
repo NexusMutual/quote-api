c76f9a0545e665361da0814d8aa645b7357b7a3e
Dan Octavian <danoctavian91@gmail.com>
2020-07-21 16:22:12 +0800

    add contract whitelist cache

--- src/contract-whitelist.js
+++ src/contract-whitelist.js
@@ -1,10 +1,27 @@
+const NodeCache = require( "node-cache" );
 const axios = require('axios');
 
+const cache = new NodeCache({ stdTTL: 60, checkperiod: 120 });
+
+const WHITELIST_KEY = 'whitelist';
+
 async function getWhitelist () {
-  const { data } = await axios.get('https://api.nexusmutual.io/coverables/contracts.json');
-  return Object.keys(data);
+
+  let whitelist = cache.get(WHITELIST_KEY);
+  if (whitelist == undefined) {
+    whitelist = [];
+    const { data } = await axios.get('https://api.nexusmutual.io/coverables/contracts.json');
+    for (const address of Object.keys(data)) {
+      if (!data[address].disabled) {
+        whitelist.push(address.toLowerCase());
+      }
+    }
+
+    cache.set(WHITELIST_KEY, whitelist);
+  }
+  return whitelist;
 }
 
 module.exports = {
-  getWhitelist,
+  getWhitelist
 };
