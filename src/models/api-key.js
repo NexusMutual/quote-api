b275016bff1e0df76faa62451acd7aefa6a87646
shark0der <tolean.dj@gmail.com>
2020-07-23 18:46:15 +0300

    Specify collection name for api key model

--- src/models/api-key.js
+++ src/models/api-key.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const { API_KEY_COLLECTION } = process.env;
 
 const apiKeySchema = new mongoose.Schema({
   apiKey: String,
   origin: String,
 });
 
-module.exports = mongoose.model('ApiKey', apiKeySchema);
+module.exports = mongoose.model('ApiKey', apiKeySchema, API_KEY_COLLECTION);
