29541ae53bc2314ff87672d62b0cfc826935b412
Dragos Horodnic <dragos@horodnic.com>
2021-05-05 18:29:21 +0300

    Replace tabs with spaces

--- src/routes.js
+++ src/routes.js
@@ -100,13 +100,13 @@ module.exports = quoteEngine => {
       });
     }
 
-		if (contractData.type === 'token' && contractData.underlyingToken !== currency) {
-			const message = `Cover for contract ${contractAddress} only allows ${contractData.underlyingToken} as a currency`;
-			return res.status(400).send({
-				error: true,
-				message,
-			});
-		}
+    if (contractData.type === 'token' && contractData.underlyingToken !== currency) {
+      const message = `Cover for contract ${contractAddress} only allows ${contractData.underlyingToken} as a currency`;
+      return res.status(400).send({
+        error: true,
+        message,
+      });
+    }
 
     const quote = await quoteEngine.getQuote(
       contractAddress,
